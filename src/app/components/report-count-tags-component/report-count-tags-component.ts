a1479e3a32de32ee9de541a71ad9c3a20fa36ec7
Jaime-vargas <jaime.vargas@live.com.mx>
2026-03-31 20:39:10 -0600

    new service to handle indalid dates

--- src/app/components/report-count-tags-component/report-count-tags-component.ts
+++ src/app/components/report-count-tags-component/report-count-tags-component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, computed, input, signal} from '@angular/core';
+import {ReportCountDTO} from '../../interfaces/report-dto/report-count.dto';
+import {NzTagComponent} from 'ng-zorro-antd/tag';
+import {UtilityService} from '../../services/utility.service';
 
 @Component({
   selector: 'app-report-count-tags-component',
-  imports: [],
+  imports: [
+    NzTagComponent
+  ],
   templateUrl: './report-count-tags-component.html',
   styleUrl: './report-count-tags-component.css',
 })
 export class ReportCountTagsComponent {
+  constructor(private utilityService: UtilityService) {
+  }
 
+  reports = input.required<ReportCountDTO[]>();
+  activeReportCount = computed(() => {
+    const now = new Date();
+    const totalReports = this.reports().length;
+    const overdueReports = this.reports().filter(report =>
+      new Date(report.dueDate) < now
+      ).length;
+    const activeReports = totalReports - overdueReports;
+    return {
+      activeReports,
+      overdueReports,
+    };
+  })
+  activeReports = computed(() => this.activeReportCount().activeReports + " " + "ACTIVE");
+  overdueReports = computed(() => this.activeReportCount().overdueReports + " " + "OVERDUE");
+
+  tagActiveColor= computed(() =>
+    this.activeReportCount().activeReports > 0 ? this.utilityService.baseColors.yellow : this.utilityService.baseColors.gray);
+  tagOverdueColor= computed(() =>
+    this.activeReportCount().overdueReports > 0 ? this.utilityService.baseColors.red : this.utilityService.baseColors.gray);
 }
