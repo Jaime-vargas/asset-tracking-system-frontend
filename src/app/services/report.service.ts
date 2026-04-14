435f3f718186a2056de683ef29985fac983c3501
Jaime-vargas <jaime.vargas@live.com.mx>
2026-04-13 20:58:37 -0600

    added service for upload images

--- src/app/services/report.service.ts
+++ src/app/services/report.service.ts
@@ -3,6 +3,7 @@ import {ApiUrlBaseService} from './api-url-base.service';
 import {Observable} from 'rxjs';
 import {ReportTableDto} from '../interfaces/report-dto/report-table.dto';
 import {ReportDetailDto} from '../interfaces/report-dto/report-detail.dto';
+import {PhotoDto} from '../interfaces/photo-dto';
 
 @Injectable({providedIn: 'root'})
 export class ReportsService {
@@ -22,4 +23,11 @@ export class ReportsService {
     return this.api.put(`reports/${reportId}/close`, reportId);
   }
 
+  uploadPhoto(reportId:number, photos: File[]):Observable<void> {
+    const formData = new FormData();
+    photos.forEach(photo => {
+      formData.append('file', photo, photo.name );
+    })
+    return this.api.post(`reports/${reportId}/photos`, formData);
+  }
 }
