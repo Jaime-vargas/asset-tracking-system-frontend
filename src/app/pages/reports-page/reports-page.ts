404ca90a0e0914056f001d2e3fca2418f3ff082e
Jaime-vargas <jaime.vargas@live.com.mx>
2026-04-17 19:14:04 -0600

    implemented filters on reports page

--- src/app/pages/reports-page/reports-page.ts
+++ src/app/pages/reports-page/reports-page.ts
@@ -15,6 +15,8 @@ import {NzTypographyComponent} from 'ng-zorro-antd/typography';
 import {NzColDirective, NzRowDirective} from 'ng-zorro-antd/grid';
 import {NzIconDirective} from 'ng-zorro-antd/icon';
 import {NzInputDirective, NzInputPrefixDirective, NzInputWrapperComponent} from 'ng-zorro-antd/input';
+import {NzOptionComponent, NzSelectComponent} from 'ng-zorro-antd/select';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-reports-page',
@@ -28,15 +30,16 @@ import {NzInputDirective, NzInputPrefixDirective, NzInputWrapperComponent} from 
     PriorityTagsComponent,
     SingleStatusTagsComponent,
     RouterLink,
-    NzBreadCrumbComponent,
-    NzBreadCrumbItemComponent,
     NzTypographyComponent,
     NzRowDirective,
     NzColDirective,
     NzIconDirective,
     NzInputDirective,
     NzInputPrefixDirective,
-    NzInputWrapperComponent
+    NzInputWrapperComponent,
+    NzOptionComponent,
+    NzSelectComponent,
+    FormsModule
   ],
   templateUrl: './reports-page.html',
   styleUrl: './reports-page.css',
@@ -57,9 +60,74 @@ export class ReportsPage {
       }
     })
   });
-  filteredReports = computed(()=>{
-    return this.reportView();
+
+  // TABLE INPUT FILTERS
+  clientList = computed(()=>
+    new Set(this.reportsData().map((report) =>
+      report.clientName
+    ))
+  )
+  branchList = computed(()=>{
+    const clientfilter = this.clientFilter()
+    return new Set(this.reportsData().filter(report =>
+      (!clientfilter || report.clientName === clientfilter)).map(report =>
+      report.branchName));
   })
+  priorityList = computed(()=>
+    new Set(this.reportsData().map((report) =>
+      report.priority))
+  )
+  statusList = computed(()=>
+    new Set(this.reportsData().map((report) =>
+      report.status))
+  )
+
+
+  idFilter = signal<number | null>(null);
+  tittleFilter = signal<string>('');
+  clientFilter = signal<string>('');
+  branchFilter = signal<string>('');
+  hardwareFilter = signal<string>('');
+  priorityFilter = signal<string>('');
+  createdAtFilter = signal<string>('');
+  dueDateFilter = signal<string>('');
+  statusFilter = signal<string>('');
+  filteredReports = computed(()=>
+    this.reportView().filter(report => {
+      const idFilter = this.idFilter();
+      const titleFilter = this.tittleFilter();
+      const clientFilter = this.clientFilter();
+      const branchFilter = this.branchFilter();
+      const hardwareFilter = this.hardwareFilter();
+      const priorityFilter = this.priorityFilter();
+      const createdDateFilter = this.createdAtFilter();
+      const dueDateFilter = this.dueDateFilter();
+      const statusFilter = this.statusFilter();
+
+      const matchId = (idFilter === null || report.id.toString().includes(idFilter.toString()));
+      const matchTitle = report.title.toLowerCase().includes(titleFilter.toLowerCase());
+      const matchClient = (!clientFilter || report.clientName === clientFilter );
+      const matchBranch = (!branchFilter || report.branchName === branchFilter);
+      const matchHardware = report.hardwareName.toLowerCase().includes(hardwareFilter.toLowerCase());
+      const matchPriority = (!priorityFilter || report.priority === priorityFilter);
+      const matchCreatedDate = report.createdDate.toDateString().toLowerCase().includes(createdDateFilter.toLowerCase());
+      const matchDueDate = report.dueDate.toDateString().toLowerCase().includes(dueDateFilter.toLowerCase());
+      const matchStatus = (!statusFilter || report.status === statusFilter);
+
+      return (matchId &&
+          matchTitle &&
+          matchClient &&
+          matchBranch &&
+          matchHardware &&
+          matchPriority &&
+          matchCreatedDate &&
+          matchDueDate &&
+          matchStatus
+      );
+    })
+
+
+  )
 
   getReports(){
     return this.reportsService.getAllReports().subscribe({
@@ -67,4 +135,6 @@ export class ReportsPage {
         return this.reportsData.set(data); },
     })
   }
+
+  protected readonly unescape = unescape;
 }
