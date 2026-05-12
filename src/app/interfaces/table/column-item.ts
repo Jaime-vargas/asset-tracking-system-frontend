ddaa7121df283664f2071a8f235a9921c1229da7
Jaime-vargas <jaime.vargas@live.com.mx>
2026-05-12 08:31:48 -0600

    types on column interface now fixed

--- src/app/interfaces/table/column-item.ts
+++ src/app/interfaces/table/column-item.ts
@@ -4,7 +4,7 @@ export interface ColumnItem {
   key:string,
   label: string;
   colWidth: number;
-  type: string;
+  type: 'button'|'icon-branch'|'icon-hardware'|'report-count-tag'|'string';
   sortOrder: NzTableSortOrder | null;
   sortFn: NzTableSortFn<Record<string, any>> | null;
   sortDirections: NzTableSortOrder[];
