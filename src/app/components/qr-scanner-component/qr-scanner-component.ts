b0bf88a21708ea5de86e7ce071359d4a25e90538
Jaime-vargas <jaime.vargas@live.com.mx>
2026-06-17 09:59:25 -0600

    testing on qr component

--- src/app/components/qr-scanner-component/qr-scanner-component.ts
+++ src/app/components/qr-scanner-component/qr-scanner-component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, signal, ViewChild} from '@angular/core';
+import {BrowserMultiFormatReader} from '@zxing/browser';
 
 @Component({
   selector: 'app-qr-scanner-component',
@@ -6,6 +7,43 @@ import { Component } from '@angular/core';
   templateUrl: './qr-scanner-component.html',
   styleUrl: './qr-scanner-component.css',
 })
-export class QrScannerComponent {
 
+export class QrScannerComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('video', { static: true })
+  video!: ElementRef<HTMLVideoElement>;
+
+  qrResult = signal<string>('');
+  devices = signal('devices');
+
+  private codeReader = new BrowserMultiFormatReader();
+
+  async ngAfterViewInit(): Promise<void> {
+
+    try {
+      const result =
+        await this.codeReader.decodeFromVideoDevice(
+          undefined,
+          this.video.nativeElement,
+          (result, error) => {
+
+            if (result) {
+
+              this.qrResult.set(result.getText());
+
+              console.log(result.getText());
+
+            }
+
+          }
+        );
+
+    } catch (error) {
+      console.error('Error al escanear', error);
+    }
+  }
+
+  ngOnDestroy(): void {
+
+  }
 }
