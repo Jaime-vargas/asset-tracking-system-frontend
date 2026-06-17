d534ceb770545b2e0098ead6b7dffcd03ec14ce0
Jaime-vargas <jaime.vargas@live.com.mx>
2026-06-16 21:14:22 -0600

    created branch form component

--- src/app/components/forms/branch-form/branch-form.ts
+++ src/app/components/forms/branch-form/branch-form.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, computed, OnDestroy, OnInit, signal} from '@angular/core';
+import {BranchService} from '../../../services/branch.service';
+import {SidebarStore} from '../../../store/sidebar.store';
+import {BranchStore} from '../../../store/branch.store';
+import {NzMessageService} from 'ng-zorro-antd/message';
+import {NzNotificationService} from 'ng-zorro-antd/notification';
+import {Subscription} from 'rxjs';
+import {ClientTableDto} from '../../../interfaces/client-table.dto';
 
 @Component({
   selector: 'app-branch-form',
@@ -6,6 +13,21 @@ import { Component } from '@angular/core';
   templateUrl: './branch-form.html',
   styleUrl: './branch-form.css',
 })
-export class BranchForm {
+export class BranchForm implements OnInit, OnDestroy {
+  constructor(private branchService: BranchService,
+              private branchStore: BranchStore,
+              private message : NzMessageService,
+              private notification : NzNotificationService,
+              private sidebarStore: SidebarStore) {
+  }
+  private subscriptions: Subscription = new Subscription();
+  ngOnInit() {}
+  ngOnDestroy() {}
 
+  branchToEdit = signal<ClientTableDto | null>(null);
+  formMode = signal<string>("add")
+
+  formTitle= computed(()=>
+    this.formMode() === "add" ? "New Client" : "Update Client"
+  )
 }
