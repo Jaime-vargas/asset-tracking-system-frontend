2908f47c25841a4c0b08eeb684c9be065a5f36ba
Jaime-vargas <jaime.vargas@live.com.mx>
2026-06-17 09:54:41 -0600

    created new components for qr reader test, login, and auth services

---
+++ src/app/components/qr-scanner-component/qr-scanner-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-qr-scanner-component',
+  imports: [],
+  templateUrl: './qr-scanner-component.html',
+  styleUrl: './qr-scanner-component.css',
+})
+export class QrScannerComponent {
+
+}
