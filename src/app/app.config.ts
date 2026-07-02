b720fb153663f29f758d0d2dbe122595372c57f1
Jaime-vargas <jaime.vargas@live.com.mx>
2026-07-02 17:08:33 -0600

    provided an interceptor in order to use authentication

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,5 @@
 import {ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners} from '@angular/core';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,11 +9,13 @@ import en from '@angular/common/locales/en';
 
 import { NzModalModule }
   from 'ng-zorro-antd/modal';
+import {authInterceptorService} from './services/auth-interceptor.service';
 
 registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(withInterceptors([authInterceptorService])),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes), provideNzI18n(en_US),
     importProvidersFrom(
