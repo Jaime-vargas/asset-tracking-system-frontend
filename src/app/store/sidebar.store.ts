d4889a6cf70ea435d02e845db6c32e2f982f7033
Jaime-vargas <jaime.vargas@live.com.mx>
2026-07-03 11:48:39 -0600

    improvement: sidebarstore don't handle other entities enymore

--- src/app/store/sidebar.store.ts
+++ src/app/store/sidebar.store.ts
@@ -1,4 +1,4 @@
-import {Injectable} from '@angular/core';
+import {Injectable, signal} from '@angular/core';
 import {BehaviorSubject, Subject} from 'rxjs';
 
 type mode = 'add' | 'edit';
@@ -9,47 +9,5 @@ type mode = 'add' | 'edit';
 
 export class SidebarStore{
 
-  private formModeSubjet =
-    new BehaviorSubject<mode>("add");
-
-  private isOpenSubjet =
-    new BehaviorSubject<boolean>(false);
-
-  private refreshTableSubjet =
-    new Subject<void>();
-
-  private selectedEntitySubjet =
-    new BehaviorSubject<any>(null);
-
-  formMode$ = this.formModeSubjet.asObservable();
-
-  isOpen$ = this.isOpenSubjet.asObservable();
-
-  refreshTable$ = this.refreshTableSubjet.asObservable();
-
-  selectedEntity$ = this.selectedEntitySubjet.asObservable();
-
-  open(): void {
-    this.isOpenSubjet.next(true);
-  }
-  close(): void {
-    this.isOpenSubjet.next(false);
-  }
-
-  setSelectedEntity(selectedEntity: any): void {
-    this.selectedEntitySubjet.next(selectedEntity);
-  }
-
-  clearSelectedEntity(): void {
-    this.selectedEntitySubjet.next(null);
-  }
-
-  setMode(mode:mode){
-    this.formModeSubjet.next(mode);
-  }
-
-  triggerRefreshTable(){
-    this.refreshTableSubjet.next();
-  }
-
+  public isOpen = signal<boolean>(false)
 }
