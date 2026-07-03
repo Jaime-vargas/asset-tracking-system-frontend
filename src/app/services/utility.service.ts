76e4d4f52304bef2da7a0a65df0ea0d0ade0293a
Jaime-vargas <jaime.vargas@live.com.mx>
2026-07-03 11:46:05 -0600

    improvements: new color on tags, new funcrion to show full date

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -8,9 +8,9 @@ export class UtilityService{
   baseColors = {
     blue: '#255498',
     gray: '#CCC',
-    green: '#428d5b',
-    red: '#c72e30',
-    yellow: '#ec8a42',
+    green: '#01ba01',
+    red: '#e75557',
+    yellow: '#febf48',
   }
 
   // THIS APPLY JUST FOR LAST UPDATE AND CLOSED AT DATES
@@ -18,6 +18,19 @@ export class UtilityService{
     if (date === "N/A"){
       return date
     }else return new Date(date).toDateString();
+  }
+
+  validLongDate(date: string):string{
+    if (date === "N/A"){
+      return date
+    }else return new Date(date).toLocaleDateString('es-MX', {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: false,
+    });
   }
 
   slugify(name: string) {
