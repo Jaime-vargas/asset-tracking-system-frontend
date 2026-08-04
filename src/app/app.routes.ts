11b80a53c3bdc56cd2ff88431860aa5b5b2c4d8e
Jaime-vargas <jaime.vargas@live.com.mx>
2026-08-04 13:46:12 -0600

    added authentication to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import {CanActivateFn, Routes} from '@angular/router';
+import {Routes} from '@angular/router';
 import {DashboardPage} from './pages/dashboard-page/dashboard-page';
 import {ClientsPage} from './pages/clients-page/clients-page';
 import {ClientBranchesPage} from './pages/client-branches-page/client-branches-page';
@@ -10,35 +10,66 @@ import {ReportsPage} from './pages/reports-page/reports-page';
 import {ReportDetailPage} from './pages/report-detail-page/report-detail-page';
 import {UsersPage} from './pages/users-page/users-page';
 import {LoginPage} from './pages/login-page/login-page';
-import {AuthService} from './services/auth-service';
 import {AuthGuardService} from './services/auth-guard.service';
 import {QrScannerComponent} from './components/qr-scanner-component/qr-scanner-component';
+import {RoleGuardService} from './services/security/role-guard.service';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginPage},
-  { path: 'dashboard', component: DashboardPage,
-  canActivate: [AuthGuardService]},
-  { path: 'clients', component: ClientsPage },
-  { path: 'clients/:clientId/:clientSlug', component: ClientBranchesPage },
-  { path: 'hardware', component: HardwarePage },
+  {
+    path: '', redirectTo: 'login', pathMatch: 'full'
+
+  },
+  {
+    path: 'login', component: LoginPage
+  },
+  {
+    path: 'dashboard', component: DashboardPage,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'clients', component: ClientsPage,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'clients/:clientId/:clientSlug', component: ClientBranchesPage,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'hardware', component: HardwarePage,
+    canActivate: [AuthGuardService]
+  },
   {
     path: 'clients/:clientId/:clientSlug/branches/:branchId/:branchSlug/hardware',
-    component: BranchHardwarePage
+    component: BranchHardwarePage,
+    canActivate: [AuthGuardService]
   },
   {
     path: 'clients/:clientId/:clientSlug/branches/:branchId/:branchSlug/hardware/:hardwareId/:hardwareSlug',
-    component: HardwareDevicePage
+    component: HardwareDevicePage,
+    canActivate: [AuthGuardService]
   },
   {
     path: 'clients/:clientId/:clientSlug/branches/:branchId/:branchSlug/hardware/:hardwareId/:hardwareSlug/reports',
-    component: HardwareReportsPage
+    component: HardwareReportsPage,
+    canActivate: [AuthGuardService]
   },
-  { path: 'reports', component: ReportsPage },
+  {
+    path: 'reports', component: ReportsPage,
+    canActivate: [AuthGuardService]
+  },
   {
     path: 'clients/:clientId/:clientSlug/branches/:branchId/:branchSlug/hardware/:hardwareId/:hardwareSlug/reports/:reportId',
-    component: ReportDetailPage
+    component: ReportDetailPage,
+    canActivate: [AuthGuardService]
   },
-  { path: 'users', component: UsersPage },
-  { path: 'qr', component: QrScannerComponent },
+  {
+    path: 'users', component: UsersPage,
+    canActivate: [AuthGuardService, RoleGuardService],
+    data: {
+     'role': 'ADMIN'
+    }
+  },
+  {
+    path: 'qr', component: QrScannerComponent
+  },
 ];
