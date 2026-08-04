a8b0ec8a8e673ea2cc941fc8023b854ef15efb58
Jaime-vargas <jaime.vargas@live.com.mx>
2026-08-04 14:01:09 -0600

    improvements on table component

--- src/app/components/table-component/table-component.ts
+++ src/app/components/table-component/table-component.ts
@@ -1,18 +1,18 @@
-import {Component, input, output} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {NzTableComponent, NzThAddOnComponent} from 'ng-zorro-antd/table';
 import {TableData} from '../../interfaces/table/table-data';
 import {ReportCountTagsComponent} from '../report-count-tags-component/report-count-tags-component';
 import {NzIconDirective} from 'ng-zorro-antd/icon';
-import {RouterLink} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {ButtonComponent} from '../button-component/button-component';
-import {NzEmptyComponent} from 'ng-zorro-antd/empty';
 import {ColumnItem} from '../../interfaces/table/column-item';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import {PriorityTagsComponent} from '../priority-tags-component/priority-tags-component';
 import {SingleStatusTagsComponent} from '../single-status-tags-component/single-status-tags-component';
 import {ApiUrlBaseService} from '../../services/api-url-base.service';
 import {NzImageDirective} from 'ng-zorro-antd/image';
-
+import {UtilityService} from '../../services/utility.service';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-table-component',
@@ -22,21 +22,23 @@ import {NzImageDirective} from 'ng-zorro-antd/image';
     NzIconDirective,
     RouterLink,
     ButtonComponent,
-    NzEmptyComponent,
     NzThAddOnComponent,
     NzTableModule,
     PriorityTagsComponent,
     SingleStatusTagsComponent,
-    NzImageDirective
+    NzImageDirective,
+    NgOptimizedImage
   ],
   templateUrl: './table-component.html',
   styleUrl: './table-component.css',
 })
 
 export class TableComponent {
-  constructor(protected apiUrlBaseService: ApiUrlBaseService) {
-  }
+  apiUrlBaseService = inject(ApiUrlBaseService);
+  router = inject(Router);
+  utilityService = inject(UtilityService);
+
+  tableLoading = input<boolean>(false);
   tableColumns = input.required<ColumnItem[]>();
   tableData = input.required<TableData[]>();
-  protected readonly console = console;
 }
